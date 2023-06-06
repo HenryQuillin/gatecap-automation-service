12633bb305e7e239545c18d0ccd184acdb058a42
Henry Quillin <henryquillin@gmail.com>
2023-06-06 11:18:06 -0500

    version not using autocomplete

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 const { getArticles } = require("./getArticles");
+const { getInfo } = require("./getInfo");
+
 
 const app = express();
 app.use(bodyParser.json({ limit: "10mb" }));
@@ -12,7 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/extract", (req, res) => {
   getArticles(req, res);
 });
+  
+app.post("/getinfo", (req, res) => {
+  getInfo(req, res);
+});
 
+// eslint-disable-next-line no-undef
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
