3c47c6d40d3bb65bf4b9a1c6093b76e2a5eff7d1
Henry Quillin <henryquillin@gmail.com>
2023-06-06 11:19:54 -0500

    adding new files

---
+++ getInfo.js
@@ -0,0 +1,139 @@
+
+
+
+async function getInfo(req, res) {
+  const Airtable = require("airtable");
+const axios = require("axios");
+
+var base = new Airtable({
+  apiKey:
+    "pat6UUeva3HgsCP0B.08d49df5c164666ce8e2415f9a3e0800bb43afbf190450b4be31cd79bccd75fd",
+}).base("appKfm9gouHkcTC42");
+  try {
+    let record = await base("Deal Flow").find(req.body.newlyAddedRecordID);
+    const recordName = record.fields["Name"];
+    let data = JSON.stringify({
+      field_ids: [
+        "identifier",
+        "short_description",
+        "website_url",
+        "twitter",
+        "linkedin",
+        "facebook",
+        "image_url",
+      ],
+      order: [
+        {
+          field_id: "rank_org",
+          sort: "asc",
+        },
+      ],
+      query: [
+        {
+          type: "predicate",
+          field_id: "identifier",
+          operator_id: "includes",
+          values: [recordName],
+        },
+        {
+          type: "predicate",
+          field_id: "facet_ids",
+          operator_id: "includes",
+          values: ["company"],
+        },
+      ],
+      limit: 50,
+    });
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "https://api.crunchbase.com/api/v4/searches/organizations",
+      headers: {
+        "X-cb-user-key": "9011e1fdbe5146865162bb45b036aa92",
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+  
+    let response = await axios.request(config);
+  
+    if (response.statusText == "OK") {
+      let data = await response.data;
+      if (data.entities && data.entities.length > 0) {
+        data = data.entities[0];
+  
+  
+        updateAirtable(record.id, data, base);
+        res.send("Success");
+  
+  
+      } else {
+        console.error("No entities found in first API response");
+      }
+    } else {
+      console.error(
+        "First API request failed:",
+        response.status,
+        response.statusText
+      );
+    }
+
+
+
+  } catch (error) {
+    console.error(error);
+  }
+
+
+
+
+
+}
+
+
+
+
+function updateAirtable(id, data, base){
+  let websiteUrl = data.properties.website_url || "";
+  let imageUrl = data.properties.image_url || "";
+  let linkedin = data.properties.linkedin.value || "";
+  let facebook = data.properties.facebook.value || "";
+  let twitter = data.properties.twitter.value || "";
+  let description = data.properties.short_description || "";
+
+  base("Deal Flow").update([
+    {
+      id: id,
+      fields: {
+        "Website URL": websiteUrl,
+        "Logo URL": imageUrl,
+        Linkedin: linkedin,
+        Facebook: facebook,
+        Twitter: twitter,
+        Description: description,
+        "Diligence Status": "Pending" ,
+        Logo: [
+          {
+            url: imageUrl,
+            filename: "Logo",
+          },
+        ],
+      },
+    },
+  ], function(err, records) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    records.forEach(function(record) {
+      console.log(record.get("Title"));
+    });
+  });
+  
+}
+
+
+module.exports = {
+  getInfo: getInfo,
+};
+2;
